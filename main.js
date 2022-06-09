7ac52ff85de8054f65176223f41ec060c1f3730d
Michael <m.double79@gmail.com>
2022-06-08 23:52:06 -0400

    code for counter JS practice project

---
+++ main.js
@@ -0,0 +1,49 @@
+const increase = document.getElementById('increase');
+const decrease = document.getElementById('decrease');
+const reset = document.getElementById('reset');
+const count = document.getElementById('count');
+let currentCount = Number(count.textContent);
+
+increase.addEventListener('click', (e) => {
+    increaseCount();
+});
+
+decrease.addEventListener('click', (e) => {
+    decreaseCount();
+});
+
+reset.addEventListener('click', (e) => {
+    resetCount();
+});
+
+increaseCount = () => {
+    currentCount = Number(currentCount += 1);
+    setCountColor(currentCount);
+    return count.innerHTML = currentCount;
+};
+
+decreaseCount = () => {
+    currentCount = Number(currentCount -= 1);
+    setCountColor(currentCount);
+    return count.innerHTML = currentCount;
+};
+
+resetCount = () => {
+    currentCount = 0;
+    setCountColor(currentCount);
+    return count.innerHTML = currentCount;
+};
+
+setCountColor = (currentCount) => {
+    console.log(currentCount);
+
+    if (currentCount === 0) {
+        count.style.color = 'darkgray';
+    } 
+    if (currentCount >= 1 ) {
+        count.style.color = 'green';
+    } 
+    if (currentCount < 0) {
+        count.style.color = 'red';
+    }
+};
